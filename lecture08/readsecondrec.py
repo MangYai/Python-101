7d5c02d8584f3b7d8ba19c5667b43ed1d50f79c7
MangYai <zsajkllam@gmail.com>
2026-09-04 01:09:24 +0700

    HW3

--- lecture08/readsecondrec.py
+++ lecture08/readsecondrec.py
@@ -10,4 +10,4 @@ with open('record.bin', 'rb') as file:
     record = struct.unpack(record_format, data)
     record = (record[0], record[1].decode().strip('\x00'), record[2], record[3])
     
-    print(f"ID: {record[0]}, Name: {record[1]}, Age: {record[2]}, GPA: {record[3]}")
+print(f"ID: {record[0]}, Name: {record[1]}, Age: {record[2]}, GPA: {record[3]}")
